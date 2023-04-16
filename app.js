a82cb271a0d28d424df94e018caf285cc35a4551
John Doe <johndoe@example.com>
2023-04-16 21:25:43 +0200

    missing: food, tagline and brew

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
@@ -9,15 +8,47 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+// Definir función para obtener lista de cervezas
+const getBeers = () => {
+  return punkAPI
+    .getBeers()
+    .then(beers => {
+      return beers;
+    })
+    .catch(error => console.log(error));
+};
 
-// ...
+// Definir función para obtener una cerveza aleatoria
+const getRandomBeer = () => {
+  return punkAPI
+    .getRandom()
+    .then(beers => {
+      return beers[0];
+    })
+    .catch(error => console.log(error));
+};
 
-// Add the route handlers here:
+// Definir ruta para renderizar vista "beers" con lista de cervezas
+app.get('/beers', (req, res, next) => {
+  getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
 
+// Definir ruta para renderizar vista de cerveza aleatoria
+app.get('/random-beer', (req, res, next) => {
+  getRandomBeer()
+    .then(randomBeer => {
+      res.render('random-beer', { randomBeer });
+    })
+    .catch(error => console.log(error));
+});
+
+// Definir ruta para renderizar vista principal
 app.get('/', (req, res) => {
   res.render('index');
 });
